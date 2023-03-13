33f6fe38564bddd1195b217ab082f943edf113eb
Arrow234 <forgit@faust3x3.com>
2023-03-13 20:47:20 +0300

    fixfinal

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -9,20 +9,29 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import javax.naming.ldap.Control;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class WebTest {
 
-
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
     @Test
     void checkForm() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Нальчик");
         $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys("08.03.2023");
+        String planningDate = generateDate(5);
+        $("[data-test-id=date] input").sendKeys(planningDate);
         $("[data-test-id=name] input").setValue("Иванова Анна-Мария");
         $("[data-test-id=phone] input").setValue("+79754568201");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(visible);
     }
 }
